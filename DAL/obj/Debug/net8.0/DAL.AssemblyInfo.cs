756d6622d033c29ed62a0db7dcc033c245421f9d
Ermek <etaldybaev1975@gmail.com>
2024-07-11 18:00:03 +0600

    save dal changes

--- DAL/obj/Debug/net8.0/DAL.AssemblyInfo.cs
+++ DAL/obj/Debug/net8.0/DAL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DAL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b4c74707123102be100aeebdb2d4ac0148a573ca")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d1bc7f9e85400520954ac983cd672e2b889ec11")]
 [assembly: System.Reflection.AssemblyProductAttribute("DAL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DAL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
